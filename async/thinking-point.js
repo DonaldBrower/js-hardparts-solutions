53390f3d3f0e0de1f80e1e39b4538c5ea00fae4d
Donald Brower <donald.brower1@gmail.com>
2021-06-22 14:01:57 -0700

    typo

--- async/thinking-point.js
+++ async/thinking-point.js
@@ -3,12 +3,12 @@ Thinking point (no writing code necessary for this challenge): Inspect the code 
 */
 
 function sayHowdy() {
-  console.log('Howdy');
+  console.log("Howdy");
 }
 
 function testMe() {
   setTimeout(sayHowdy, 0);
-  console.log('Partnah');
+  console.log("Partnah");
 }
 
 testMe(); // what order should these log out? Howdy or Partnah first?
@@ -19,3 +19,4 @@ testMe(); // what order should these log out? Howdy or Partnah first?
 > Howdy
 
 because setTimeout is put into the callback queue, it cannot execute it's code until all the code in the global call stack is run, because you cannot have asynchronous actions happening while synchronous code is being run, because that's just not how it works
+*/

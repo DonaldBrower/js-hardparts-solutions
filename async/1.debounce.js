e25bd8cd8d55ad9aa14da5ff7de3530da782e420
Donny Brower <donald.brower1@gmail.com>
2021-08-04 10:18:09 -0700

    Update 1.debounce.js
    hoist refactor

--- async/1.debounce.js
+++ async/1.debounce.js
@@ -1,43 +1,6 @@
-function debounce(callback, interval) {
-  let timer,
-    readyToCall = true;
+"use strict";
 
-  /**
-   *
-   * @returns callback(), or "undefined" depending on if interval ms have ellapsed since last call to debouncedFunction
-   */
-  function debouncedFunction() {
-    if (readyToCall) {
-      readyToCall = false;
-
-      timer = setTimeout(() => {
-        readyToCall = true;
-      }, interval);
-
-      return callback();
-    } else {
-      // when debouncedFunction is not ready to be called,the timer running from the previous call should be reset—making it "debouced three seconds from every call", instead of "debounded three seconds from every successful call"
-      clearTimeout(timer);
-
-      timer = setTimeout(() => {
-        readyToCall = true;
-      }, interval);
-
-      return "unsucessful";
-    }
-  }
-
-  return debouncedFunction;
-}
-
-function sayHi() {
-  return "hi";
-}
-
-const debouncedSayHi = debounce(sayHi, 3000);
-
-/* tests */
-
+var debouncedSayHi = debounce(sayHi, 3000);
 console.log(debouncedSayHi()); // -> 'hi'
 
 setTimeout(() => {
@@ -52,4 +15,38 @@ setTimeout(() => {
   console.log(debouncedSayHi());
 }, 8000); // -> 'hi'
 
-// ?? using iterm to run file, there is a second pause in between when cb for last setTimeout? I would expect it to just cut out right there
+function sayHi() {
+  return "hi";
+}
+
+//*************************************
+function debounce(callback, interval) {
+  var timer,
+    readyToCall = true;
+  
+  // @returns callback(), or "undefined" depending on if interval ms have ellapsed since last call to debouncedFunction
+  function debouncedFunction() {
+    if (readyToCall) {
+      readyToCall = false;
+
+      timer = setTimeout(() => {
+        readyToCall = true;
+      }, interval);
+
+      return callback();
+    } else {
+      // when debouncedFunction is not ready to be called,the timer running from the 
+      // previous call should be reset—making it "debouced three seconds from every call", 
+      // instead of "debounded three seconds from every successful call"
+      clearTimeout(timer);
+
+      timer = setTimeout(() => {
+        readyToCall = true;
+      }, interval);
+
+      return "unsucessful";
+    }
+  }
+
+  return debouncedFunction;
+}
